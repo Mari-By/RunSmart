ee659a15c3add27b62e667034bcc33f4dc1e09f2
Mary <mariya.sedna@gmail.com>
2024-06-10 21:38:23 -0300

    slider adaptation beginning

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,17 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 300,
        /*  adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/icon_chevron-left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/icon_chevron-right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/icon_chevron-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/icon_chevron-right.png"></button>',
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+                
+        }
+        }
+        ]
     })
 });
