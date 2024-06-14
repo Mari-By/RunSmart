6f9070a53c672d74082ca71887a91a8271f1a148
Mary <mariya.sedna@gmail.com>
2024-06-14 19:43:36 -0300

    js catalog in work

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,27 @@ $(document).ready(function(){
         }
         ]
     })
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      $('.catalog__link').each(function(i) {
+        $(this).on('click', function(e) {
+           e.preventDefault();
+           $('.catalog__main-content').eq(i).toggleClass('.catalog__main-content_active');
+           $('.catalog__list-content').eq(i).toggleClass('.catalog__list-content_active');
+        })
+      })
+
+      $('.catalog__link-list').each(function(i) {
+        $(this).on('click', function(e) {
+           e.preventDefault();
+           $('.catalog__main-content').eq(i).toggleClass('.catalog__main-content_active');
+           $('.catalog__list-content').eq(i).toggleClass('.catalog__list-content_active');
+        })
+      })
+
 });
