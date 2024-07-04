7340466ed52df959911d83722cc15e1cde753534
Mary <mariya.sedna@gmail.com>
2024-07-04 15:03:44 -0300

    php is ready (+-)

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                 // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;           // Enable SMTP authentication
 $mail->Username = 'web.testing.mb@gmail.com';        // Наш логин (эл.почта)
-$mail->Password = 'hgestyjuinfcwqov';        // Наш пароль от ящика
+$mail->Password = 'hges tyju infc wqov';        // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';     // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;           // TCP port to connect to
  
