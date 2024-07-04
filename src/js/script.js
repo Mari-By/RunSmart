0cb4f65d71287a0824d5a2987e9b39379ad3e16c
Mary <mariya.sedna@gmail.com>
2024-07-04 16:30:01 -0300

    pageUp+scroll

--- src/js/script.js
+++ src/js/script.js
@@ -178,4 +178,19 @@ $(document).ready(function(){
           return false;
         });
 
+        /* Плавный скроллинг в начало страницы */
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 1200) {
+            $('.pageup').fadeIn();
+          } else {
+            $('.pageup').fadeOut();
+          }
+        });
+
+        $("a[href^='#']").click(function() {
+          var _href = $(this).attr("href");
+          $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+          return false;
+        });
+
 });
